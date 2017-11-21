2f37541457c385f78c1341d6048bfe46bd424964
chaoyu chen <fatchaoyu@gmail.com>
2017-11-21 20:43:49 +0800

    add user task file

---
+++ STM32L746-DISC/Inc/user_tasks.h
@@ -0,0 +1,13 @@
+/* File name: user_tasks.h
+ *
+ * Interfaces of user tasks are declared here.
+ *
+*/
+
+#ifndef __user_task_H
+#define __user_task_H
+
+/* LCD task*/
+void StartLcdTask(void const * argument);
+
+#endif //__user_task_H
